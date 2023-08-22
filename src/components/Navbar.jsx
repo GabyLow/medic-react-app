c220b47bd8899b1cbb8fdc6efa7ab3f6315bbfc6
GabyLow <127358083+GabyLow@users.noreply.github.com>
2023-08-21 18:03:24 -0600

    :sparkles:feat(add):style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,30 @@
 import { Link } from "react-router-dom";
+import logo from '../assets/logo-md-2.png';
 
 export const Navbar = () => {
   return (
     <nav className="bg-gradient-to-r from-cyan-500 to-blue-500">
       <div className="flex justify-between items-center px-4 py-6">
-        <h1 className="text-white font-bold">My Medic App</h1>
+        <div className="flex items-center">
+          <img src={logo} alt="Logo" className="w-20 h-15 mr-2" /> {/* Ajusta el tamaño y margen según sea necesario */}
+          <h1 className="text-white text-xl font-extrabold">My Medic App</h1>
+        </div>
         <ul className="flex">
           <li>
-            <Link to="/" className="text-white hover:text-gray-400 px-4 py-2"> Home </Link>
+            <Link to="/" className="font-bold text-white hover:text-gray-400 px-4 py-2"> Home </Link>
           </li>
           <li>
-            <Link to="/create" className="text-white hover:text-gray-400 px-4 py-2"> Create Patients </Link>
+            <Link to="/create" className="font-bold text-white hover:text-gray-400 px-4 py-2"> Create Patients </Link>
           </li>
           <li>
-            <Link to="/about" className="text-white hover:text-gray-400 px-4 py-2"> About </Link>
+            <Link to="/about" className="font-bold text-white hover:text-gray-400 px-4 py-2"> About </Link>
           </li>
           <li>
-            <Link to="/patients" className="text-white hover:text-gray-400 px-4 py-2"> Patients </Link>
+            <Link to="/patients" className="font-bold text-white hover:text-gray-400 px-4 py-2"> Patients </Link>
           </li>
         </ul>
       </div>
     </nav>
   );
 };
+
--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -2,10 +2,61 @@ import { Navbar } from "../components/Navbar"
 
 
 const CreatePage = () => {
+
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const patientName = formData.get('name');
+    const patientAge = formData.get('age');
+    console.log(patientName);
+    console.log(patientAge);
+  }
+  
+  
+
   return (
     <>
      <Navbar/>
      <h1 className="text-3xl font-bold underline"> Form </h1>
+     <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+            <div className="md:flex">
+                <div className='p-8 w-full'>
+                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold mb-1"> Form to add patient</div>
+                    <form onSubmit= {handleSubmit}>
+                        <input 
+                            className='shadow appearance-none border rounded w-full m-3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' 
+                            type="text" 
+                            name="name" 
+                            placeholder='Patient Name'
+                        />
+
+                        <input 
+                            className='shadow appearance-none border rounded w-full m-3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' 
+                            type="number" 
+                            name="age" 
+                            placeholder='Patient Age'
+                        />
+
+                        <input 
+                            className='shadow appearance-none border rounded w-full m-3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' 
+                            type="number" 
+                            name="num_afi" 
+                            placeholder='Patient affiliation number' 
+                        />
+
+                        <textarea className='shadow appearance-none border rounded m-3 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                            name="address"
+                            cols="30" 
+                            rows="10"
+                            placeholder='Patient Address'
+                        ></textarea>
+
+                        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type='submit'>Add Patient</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
